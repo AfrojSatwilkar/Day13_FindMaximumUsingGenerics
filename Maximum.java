f417d111d819ddf441da447c5dec06fcbe2e73d8
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-10-14 23:48:30 +0530

    single generic method for all data types

--- Maximum.java
+++ Maximum.java
@@ -1,9 +1,19 @@
 package com.company1.Day13_MaximumProblemUsingGenerics;
 
-public class Maximum {
+public class Maximum<E extends Comparable<E>> {
+    E a, b, c;
+    public Maximum(E a,E b,E c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
 
-    public static String findMaximum(String a, String b, String c) {
-        String max = a;
+    public E findMaximum(){
+        return Maximum.findMaximum(a,b,c);
+    }
+
+    public static <E extends Comparable<E>>E findMaximum(E a, E b, E c) {
+        E max = a;
         if (b.compareTo(max)>0 && b.compareTo(c)>0) {
             max = b;
         }
@@ -15,8 +25,11 @@ public class Maximum {
 
     public static void main(String[] args) {
         System.out.println("Welcome Generics Maximum program");
+        Integer xInt = 5, yInt = 9, zInt = 3;
+        Float xFloat = 6.2f, yFloat = 4.7f, zFloat = 7.6f;
         String xString = "Apple", yString = "Peach", zString = "Banana";
-        String getMax = Maximum.findMaximum(xString,yString,zString);
-        System.out.print(getMax);
+        System.out.println("Maximum Integer : "+ new Maximum(xInt,yInt,zInt).findMaximum());
+        System.out.println("Maximum Float : "+ new Maximum(xFloat,yFloat,zFloat).findMaximum());
+        System.out.println("Maximum String : "+ new Maximum(xString,yString,zString).findMaximum());
     }
 }
