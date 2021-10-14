cacb55d7d5eafec6a86e75324e81d73dc651dc69
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-10-14 13:56:20 +0530

    Welcome to generics maximum program

---
+++ Maximum.java
@@ -0,0 +1,7 @@
+package com.company1.Day13_MaximumProblemUsingGenerics;
+
+public class Maximum {
+    public static void main(String[] args) {
+        System.out.println("Welcome Generics Maximum program");
+    }
+}
