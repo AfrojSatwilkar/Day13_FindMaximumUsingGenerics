5bd0d26de7d59f72bb67142f25d0f435ddbcfaec
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-10-14 14:05:17 +0530

    three integer find maximum

--- Maximum.java
+++ Maximum.java
@@ -1,7 +1,21 @@
 package com.company1.Day13_MaximumProblemUsingGenerics;
 
 public class Maximum {
+
+    public static Integer findMaximum(Integer a, Integer b, Integer c) {
+        Integer max = a;
+        if (b.compareTo(max)>0 && b.compareTo(c)>0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome Generics Maximum program");
+        int getMax = Maximum.findMaximum(11,9,6);
+        System.out.print(getMax);
     }
 }
