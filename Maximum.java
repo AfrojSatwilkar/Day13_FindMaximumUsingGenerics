af8a9e5405d8cc21bc5fd95467981bd3f4688593
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-10-15 00:40:21 +0530

    declare generic class

--- Maximum.java
+++ Maximum.java
@@ -3,14 +3,12 @@ package com.company1.Day13_MaximumProblemUsingGenerics;
 public class Maximum {
     public static void main(String[] args) {
         System.out.println("Welcome Generics Maximum program");
-        Integer xInt = 5, yInt = 9, zInt = 3;
+        Integer xInt = 15, yInt = 9, zInt = 3;
         Float xFloat = 6.2f, yFloat = 4.7f, zFloat = 7.6f;
         String xString = "Apple", yString = "Peach", zString = "Banana";
 
-        Refactor1 refactor1 = new Refactor1<Integer>();
-
-        System.out.println("Maximum Integer : "+ refactor1.findMaximum(xInt,yInt,zInt));
-        System.out.println("Maximum Float : "+ refactor1.findMaximum(xFloat,yFloat,zFloat));
-        System.out.println("Maximum String : "+ refactor1.findMaximum(xString,yString,zString));
+        new Refactor1(xInt,yInt,zInt).findMaximum();
+        new Refactor1(xFloat,yFloat,zFloat).findMaximum();
+        new Refactor1(xString,yString,zString).findMaximum();
     }
 }
--- Refactor1.java
+++ Refactor1.java
@@ -1,6 +1,18 @@
 package com.company1.Day13_MaximumProblemUsingGenerics;
 
-public class Refactor1 <E>{
+public class Refactor1 <E extends Comparable<E>>{
+    E a, b, c;
+
+    public Refactor1(E a, E b, E c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public E findMaximum(){
+        return Refactor1.findMaximum(a,b,c);
+    }
+    
     public static <E extends Comparable<E>>E findMaximum(E a, E b, E c) {
         E max = a;
         if (b.compareTo(max)>0 && b.compareTo(c)>0) {
@@ -9,6 +21,11 @@ public class Refactor1 <E>{
         if (c.compareTo(max) > 0) {
             max = c;
         }
+        printMax(max);
         return max;
+    }
+
+    public static <E> void printMax(E max){
+        System.out.println("Maximum : "+max);
     }
 }
